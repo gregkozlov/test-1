1051c6e8ec73bf3cfc4ac137fe416bd62fd3678f
Artur Cherkanov <>
2022-12-28 00:41:50 +0300

    feat: add nav translations for nav component

--- src/shared/components/ToggleButton/types.d.ts
+++ src/shared/components/ToggleButton/types.d.ts
@@ -2,4 +2,6 @@ type ToggleType = {
   onClick: (isToggled: boolean) => void;
   label?: string;
   toggled: boolean;
+  rightPlaceholeder?: React.ReactNode;
+  leftPlaceholder?: React.ReactNode;
 };
