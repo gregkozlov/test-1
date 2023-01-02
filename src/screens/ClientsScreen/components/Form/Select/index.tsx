2a3e7a9401d66b1b4d8dc45b96b3c69f7f9c8882
Pavel Harmaza <pa.harmaza@wscloud.pro>
2023-01-02 19:05:19 +0300

    feat: add select component

---
+++ src/screens/ClientsScreen/components/Form/Select/index.tsx
@@ -0,0 +1,34 @@
+import { useTheme } from "../../../../../shared/hooks";
+import { ChevronDown } from "../../../../../shared/images";
+import styles from "./select.module.scss";
+import { SelectProps } from "./types";
+
+const Select: React.FC<SelectProps> = ({
+  size = "medium",
+  label = "",
+  disabled = false,
+  placeholder = "",
+
+  options
+}) => {
+  const theme = useTheme();
+  return (
+    <div className={styles.select}>
+      {label && <h2 className={`${styles.select__label} ${styles[theme]}`}>{label}</h2>}
+      <div className={styles.relative}>
+        <select className={`${styles.field} ${styles[size]} ${styles[theme]} `} disabled={disabled}>
+          <option value="" className={styles.field__placeholder} disabled selected hidden>
+            {placeholder}
+          </option>
+          {options.map((el) => (
+            <option value={el} key={el}>
+              {el}
+            </option>
+          ))}
+        </select>
+        <ChevronDown className={styles.field__chevron} />
+      </div>
+    </div>
+  );
+};
+export default Select;
---
+++ src/screens/ClientsScreen/components/Form/Select/types.ts
@@ -0,0 +1,7 @@
+export type SelectProps = {
+  size?: "small" | "medium" | "large";
+  disabled?: boolean;
+  placeholder?: string;
+  label?: string;
+  options: string[];
+};
